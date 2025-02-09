c695f9bfe84d05a44c257df1a33e934812eb5110
Endriw Braga <endriwbragalisboa@gmail.com>
2025-02-09 11:30:38 -0300

    Mudança na aba de contato, refatoração css, correção bug js menu mobile

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav__suprime");
+const links = document.querySelectorAll(".nav__links a");
 
-hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+if (hamburger && nav) {
+  hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+  links.forEach(link => {
+    link.addEventListener("click", () => nav.classList.remove("active"));
+  });
+}
 

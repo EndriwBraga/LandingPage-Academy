fa172f73fd305a420d7029fe37ab4f77ca0b406d
Endriw Braga <endriwbragalisboa@gmail.com>
2025-02-10 09:24:29 -0300

    Adicionei um carrosel na aba de cards, irei adicionar mais alguns cards para ficar mais dinamico e atrativo

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav__suprime");
 const links = document.querySelectorAll(".nav__links a");
 
+const track = document.querySelector(".explore__grid");
+const nextCard = document.querySelector(".next");
+const prevCard = document.querySelector(".prev");
+
+const totalCards = document.querySelectorAll(".explore__card").length;
+
 if (hamburger && nav) {
   hamburger.addEventListener("click", () => nav.classList.toggle("active"));
 
@@ -10,3 +16,29 @@ if (hamburger && nav) {
   });
 }
 
+let currentIndex = 0;
+const cardWidth = 268 + 32; // 268px de largura + 2rem (32px) de gap convertido para pixels
+
+
+function updateCarousel() {
+  track.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
+}
+
+nextCard.addEventListener("click", () => {
+  if(currentIndex < totalCards -3){
+    currentIndex++;
+    updateCarousel();
+  }
+});
+
+prevCard.addEventListener("click", () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateCarousel();
+  }
+});
+
+console.log(nextCard);
+console.log(prevCard);
+console.log(totalCards);
+console.log(cardWidth);
